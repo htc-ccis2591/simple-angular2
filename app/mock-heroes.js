bbdbc3a660e7b7d3fc783a9ef861557559a2c4c4
Ryan Freemark <freemark21@gmail.com>
2016-04-13 17:40:34 -0500

    Finished

---
+++ app/mock-heroes.js
@@ -0,0 +1,26 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 11, "name": "Driver" },
+                { "id": 12, "name": "Putter" },
+                { "id": 13, "name": "Lob Wedge" },
+                { "id": 14, "name": "Sand Wedge" },
+                { "id": 15, "name": "Pitching Wedge" },
+                { "id": 16, "name": "9 iron" },
+                { "id": 17, "name": "8 iron" },
+                { "id": 18, "name": "7 iron" },
+                { "id": 19, "name": "6 iron" },
+                { "id": 20, "name": "5 iron" },
+                { "id": 21, "name": "4 hybrid" },
+                { "id": 22, "name": "7 wood" },
+                { "id": 23, "name": "3 wood" }
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+	{"id": 11, "name": "Driver"},
+	{"id": 12, "name": "Putter"},
+	{"id": 13, "name": "Lob Wedge"},
+	{"id": 14, "name": "Sand Wedge"},
+	{"id": 15, "name": "Pitching Wedge"},
+	{"id": 16, "name": "9 iron"},
+	{"id": 17, "name": "8 iron"},
+	{"id": 18, "name": "7 iron"},
+	{"id": 19, "name": "6 iron"},
+	{"id": 20, "name": "5 iron"},
+    {"id": 21, "name": "4 hybrid"},
+    {"id": 22, "name": "7 wood"},
+    {"id": 23, "name": "3 wood"}
+];
