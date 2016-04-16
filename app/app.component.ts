b04208f2c0f2c0c53a3195973b00851273c04090
David Gossell <itpro@gossellmn.com>
2016-04-16 16:37:41 -0500

    Finished tutorial, made minor style adjustments to Dashboard and Home links to make them look like buttons.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,20 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
 import { HeroService }     from './hero.service';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
+  <p>
   <a [routerLink]="['Dashboard']">Dashboard</a>
   <a [routerLink]="['Heroes']">Heroes</a>
+  </p>
   <router-outlet></router-outlet>
 `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
@@ -22,6 +26,11 @@ import { DashboardComponent } from './dashboard.component';
 })
 @RouteConfig([
   {
+      path: '/detail/:id',
+      name: 'HeroDetail',
+      component: HeroDetailComponent
+  },
+    {
       path: '/dashboard',
       name: 'Dashboard',
       component: DashboardComponent,
